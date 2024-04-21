f1c265f93f8a01434451538ef6480019d8c5bb20
Vivek <viveks95944@gmail.com>
2024-04-21 11:59:23 +0530

    next_portfolio_added

---
+++ constants/data.js
@@ -0,0 +1,80 @@
+const userData = {
+  githubUsername: "vivekx01",
+  name: "Vivek Shukla",
+  designation: "Software Engineer",
+  avatarUrl: "/avatar.jpg",
+  email: "viveks95944@gmail.com",
+  phone: "+91 9619037203",
+  address: "Mumbai, Maharashtra, India.",
+  projects: [
+    {
+      title: "Investocrypt",
+      link: "https://investo-crypt.web.app",
+      imgUrl: "/investocrypt.png",
+    },
+    {
+      title: "Blockchain Voting System",
+      link: "https://blockchain-election.web.app/",
+      imgUrl: "/blockchain-voting-system.png",
+    },
+    {
+      title: "Scrapmart",
+      link: "https://scrapmart.onrender.com",
+      imgUrl: "/scrapmart.png",
+    },
+    
+  ],
+  about: {
+    title:
+      "I'm a software engineer that loves exploring different technologies",
+    description: [
+      `I've been learning full-stack application since past 5 years of my academic life.`,
+      `After learning HTML and struggling with CSS, I came up with a brilliant idea of using bootstrap so that I don't have to style everything by myself :P`,
+      `I've been coding in React, Angular,  Node.js, Express, Django, FastAPI, MongoDB, PostgreSQL, Bootstrap.`,
+    ],
+    currentProject: "Investocrypt",
+    currentProjectUrl: "https://investo-crypt.web.app",
+  },
+  experience: [
+    {
+      title: "Software Engineer Intern",
+      company: "DronaHQ",
+      year: "2024",
+      companyLink: "https://dronahq.com",
+      desc: "Developing, Debugging and fixing issues in the product. Mainly wokring with Vanilla JavaScript",
+    },
+    {
+      title: "Master of Computer Applications",
+      company: "TIMSCDR, Mumbai.",
+      year: "2024",
+      companyLink: "https://timscdrmumbai.in",
+      desc: "Masters in Computer Applications with an aggregate CGPA of 8. Thankfully landed me a job into the industry xD.",
+    },
+    {
+      title: "BSc - Computer Science",
+      company: "GN Khalsa College, Mumbai.",
+      year: "2022",
+      companyLink: "https://gnkhalsa.edu.in/",
+      desc: "Major in Computer Science with an aggregate CGPA of 9.67. Nobody asks this but it's okay.",
+    },
+    {
+      title: "Class XII - Science",
+      company: "GN Khalsa College, Mumbai.",
+      year: "2019",
+      companyLink: "https://gnkhalsa.edu.in/",
+      desc: "Subjects being PCMB - I barely survived with 65% marks aggregate.",
+    },
+  ],
+  resumeUrl:
+    "https://drive.google.com/file/d/19RcuVXZRX2MQjY2kMfH96x-_xl5W6Xbn/view?usp=sharing",
+  socialLinks: {
+    instagram: "https://instagram.com/vivek.x01",
+    twitter: "https://twitter.com/vivekx01",
+    linkedin: "https://linkedin.com/in/vivekx01",
+    github: "https://github.com/vivekx01",
+    facebook: "https://facebook.com/",
+    peerlist: "https://peerlist.io/",
+  },
+};
+
+export default userData;
---
+++ next.config.mjs
@@ -0,0 +1,11 @@
+/**
+* @type {import('next').NextConfig}
+*/
+const nextConfig = {
+  output: "standalone",
+  images: {
+    unoptimized: true,
+  }
+};
+
+export default nextConfig;
